815913938dda01daf606489f5984115f8faf643b
Thomas Karachristos <thomas.karachristos@ist.com.gr>
2016-05-11 09:42:52 +0300

    change code position for better understanding

--- Creational/Builder/jqueryBuilderCode.js
+++ Creational/Builder/jqueryBuilderCode.js
@@ -3,13 +3,6 @@
 // JQuery have a very effective and amazing builder object
 // a number of different means for dynamically building new jQuery (and DOM) objects,
 // by either passing in the complete markup for an element, partial markup and content or using the jQuery for construction:
-// here the jquery  builder design interface, you can see a piece of code in jqueryBuilderCode.js
-$( '<div class="foo">bar</div>' );
-$( '<p id="test">foo <em>bar</em></p>').appendTo("body");
-var newParagraph = $( "<p />" ).text( "Hello world" );
-$( "<input />" )
-      .attr({ "type": "text", "id":"sample"})
-      .appendTo("#container");
 
 // here is the code from jquery
 // HANDLE: $(html) -> $(array)
@@ -36,3 +29,11 @@ if ( match[1] ) {
   }
 
   return jQuery.merge( this, selector );
+
+// here is how you use it
+$( '<div class="foo">bar</div>' );
+$( '<p id="test">foo <em>bar</em></p>').appendTo("body");
+var newParagraph = $( "<p />" ).text( "Hello world" );
+$( "<input />" )
+      .attr({ "type": "text", "id":"sample"})
+      .appendTo("#container");
