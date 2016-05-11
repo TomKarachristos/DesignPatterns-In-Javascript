754ac6f9b3ab49d13c789aa5fcd9234685415b22
Thomas Karachristos <thomas.karachristos@ist.com.gr>
2016-05-11 13:21:28 +0300

    add decorate examples

---
+++ Structural/Decorator/decorateWithJquery.js
@@ -0,0 +1,54 @@
+var decoratorApp = decoratorApp || {};
+ 
+// define the objects we're going to use
+decoratorApp = {
+ 
+    defaults: {
+        validate: false,
+        limit: 5,
+        name: "foo",
+        welcome: function () {
+            console.log( "welcome!" );
+        }
+    },
+ 
+    options: {
+        validate: true,
+        name: "bar",
+        helloWorld: function () {
+            console.log( "hello world" );
+        }
+    },
+ 
+    settings: {},
+ 
+    printObj: function ( obj ) {
+        var arr = [],
+            next;
+        $.each( obj, function ( key, val ) {
+            next = key + ": ";
+            next += $.isPlainObject(val) ? printObj( val ) : val;
+            arr.push( next );
+        } );
+ 
+        return "{ " + arr.join(", ") + " }";
+    }
+ 
+};
+ 
+// merge defaults and options, without modifying defaults explicitly
+decoratorApp.settings = $.extend({}, decoratorApp.defaults, decoratorApp.options);
+ 
+// what we have done here is decorated defaults in a way that provides
+// access to the properties and functionality it has to offer (as well as
+// that of the decorator "options"). defaults itself is left unchanged
+ 
+$("#log")
+    .append( decoratorApp.printObj(decoratorApp.settings) +
+          + decoratorApp.printObj(decoratorApp.options) +
+          + decoratorApp.printObj(decoratorApp.defaults));
+ 
+// settings -- { validate: true, limit: 5, name: bar, welcome: function (){ console.log( "welcome!" ); },
+// helloWorld: function (){ console.log( "hello world" ); } }
+// options -- { validate: true, name: bar, helloWorld: function (){ console.log( "hello world" ); } }
+// defaults -- { validate: false, limit: 5, name: foo, welcome: function (){ console.log("welcome!"); } }
