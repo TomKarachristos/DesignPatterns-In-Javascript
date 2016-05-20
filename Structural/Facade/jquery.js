52d4682bbeb7decfdf5f8dca3531bfb380aeadcd
Tom Karachristos <TomKarachristos@users.noreply.github.com>
2016-05-20 17:10:19 +0300

    Update jquery.js

--- Structural/Facade/jquery.js
+++ Structural/Facade/jquery.js
@@ -15,21 +15,53 @@ function addEvent(el, type, fn) {
   }
 }
 
-bindReady: function() {
-  [..]
-  if ( document.addEventListener ) {
-    // Use the handy event callback
-    document.addEventListener( "DOMContentLoaded", DOMContentLoaded, false );
+function bindReady(){
+	if ( readyBound ) return;
+	readyBound = true;
 
-    // A fallback to window.onload, that will always work
-    window.addEventListener( "load", jQuery.ready, false );
+	// If Mozilla is used
+	if ( jQuery.browser.mozilla || jQuery.browser.opera )
+		// Use the handy event callback
+		document.addEventListener( "DOMContentLoaded", jQuery.ready, false );
+	
+	// If IE is used, use the excellent hack by Matthias Miller
+	// http://www.outofhanwell.com/blog/index.php?title=the_window_onload_problem_revisited
+	else if ( jQuery.browser.msie ) {
+	
+		// Only works if you document.write() it
+		document.write("<scr" + "ipt id=__ie_init defer=true " + 
+			"src=//:><\/script>");
+	
+		// Use the defer script hack
+		var script = document.getElementById("__ie_init");
+		
+		// script does not exist if jQuery is loaded dynamically
+		if ( script ) 
+			script.onreadystatechange = function() {
+				if ( this.readyState != "complete" ) return;
+				jQuery.ready();
+			};
+	
+		// Clear from memory
+		script = null;
+	
+	// If Safari  is used
+	} else if ( jQuery.browser.safari )
+		// Continually check to see if the document.readyState is valid
+		jQuery.safariTimer = setInterval(function(){
+			// loaded and complete are both valid states
+			if ( document.readyState == "loaded" || 
+				document.readyState == "complete" ) {
+	
+				// If either one are found, remove the timer
+				clearInterval( jQuery.safariTimer );
+				jQuery.safariTimer = null;
+	
+				// and execute any waiting functions
+				jQuery.ready();
+			}
+		}, 10); 
 
-  // If IE event model is used
-  } else if ( document.attachEvent ) {
-
-    document.attachEvent( "onreadystatechange", DOMContentLoaded );
-
-    // A fallback to window.onload, that will always work
-    window.attachEvent( "onload", jQuery.ready );
-  [..]
+	// A fallback to window.onload, that will always work
+	jQuery.event.add( window, "load", jQuery.ready );
 }
